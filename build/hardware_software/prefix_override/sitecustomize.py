ac674c3e402cd992a90035e6d087c9a030759c80
Ghina Debian <ghina-debian@ghina-debian-asus.lan>
2025-09-21 07:46:46 +0300

    cleaned and organized directories, deleted uneccessary folders

--- build/hardware_software/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/fyp3/Desktop/ros2_swarm_mapping/install/hardware_software'
--- robot/ros2ws/build/imu_reader/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/fyp/Desktop/swarm_repo/robot/ros2ws/install/imu_reader'
